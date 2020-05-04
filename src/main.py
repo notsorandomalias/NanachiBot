30bd1c64f9e8f8da8353ee9b92bfdae58b68f02f
shushu <golpos369@gmail.com>
2020-05-04 03:49:05 -0400

    idk what i did but i hope it works

--- src/main.py
+++ src/main.py
@@ -1,35 +1,9 @@
 from discord import File
 from discord.ext import commands
-#from boto.s3.connection import S3Connection
 import os
-#from PIL import Image, ImageDraw, ImageFont, ImageSequence
-#from io import BytesIO
 
 bot = commands.Bot(command_prefix='?')
-token = os.environ.get("NANACHIBOT_TOKEN")
-#try:
-#    token = S3Connection(os.environ["NANACHIBOT_TOKEN"])
-#except:
-#    token = os.environ["NANACHIBOT_TOKEN"]
-
-'''
-def crabwrite(file):
-    #impact = ImageFont.truetype('impact.ttf', 32)
-    frames = []
-
-    for frame in ImageSequence.Iterator(file):
-        cr = ImageDraw.Draw(frame)
-        cr.text((0, 1000), "Hello World", align='center')
-        del cr
-
-        b = BytesIO()
-        frame.save(b, format='GIF')
-        frame = Image.open(b)
-
-        frames.append(frame)
-    
-    frames[0].save('out.gif', save_all=True, append_images=frames[1:])
-'''
+token = os.environ.get("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
