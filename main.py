1c43b97a0835ebc1e6fd26c70efce0f55fd90bd0
shushu <golpos369@gmail.com>
2020-05-22 15:09:07 -0400

    fixed regex and bot stuff

--- main.py
+++ main.py
@@ -7,29 +7,25 @@ bot = commands.Bot(command_prefix='?')
 token = os.environ.get("DISCORD_TOKEN")
 
 @bot.event
-async def on_message(message):
-    if message.author == bot.user or message.author.bot:
-        return
-
-    result = re.match('\bpooger', message)
-
-    if result in message.content.lower():
-        authormention = message.author.mention
-        response = f"{authormention} please shut the fuck up."
-        await message.channel.send(response)
-
-@bot.event
 async def on_ready():
     print("Logged in as {0.user}".format(bot))
     print("Fired up and ready to go~")
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user or message.author.bot:
+        return
+
+    if re.search('(?i)\\bpooger', message.content):
+        authormention = message.author.mention
+        response = f"{authormention} please shut the fuck up."
+        await message.channel.send(response)
+    
+    await bot.process_commands(message)
+
 @bot.command()
-async def crab(ctx, text:str):
-    try:
-        await ctx.send(file=File('src/crabrave.gif'))
-        os.remove('test.gif')
-    except:
-        await ctx.send("Error: Failed to send a gif of a crab rave rendered in Unreal Engine 4 in 4k. sad crabs")
+async def crab(ctx):
+    await ctx.send(file=File('src/crabrave.gif'))
 
 @bot.command()
 async def p(ctx):
